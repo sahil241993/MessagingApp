598f33a400a6ac34df5d9e9f8fb658192ac6f352
sahil sharma <sahilmarish241993@gmail.com>
2018-03-27 07:33:48 +0530

    iii

--- config/development.js
+++ config/development.js
@@ -8,6 +8,9 @@
 module.exports={
 	
     connectionLimit : 100,
+    connectTimeout  : 60 * 60 * 1000,
+    aquireTimeout   : 60 * 60 * 1000,
+    timeout         : 60 * 60 * 1000,
     host : "ss.ciljehvh059e.us-east-2.rds.amazonaws.com",
     user :"ss", 
     password: "sahilsharma",       
